e5a5f9141cc55ba60ce8369d9a251419f1c1d0b3
stefanRitter <stefan@stefanritter.com>
2013-10-13 20:13:57 +0100

    use CSS transform instead of positioning, add placeholder text

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,14 +1,14 @@
 (function() {
   jQuery(function($) {
 
-    function toggleHome($e, txt) {
-      if ($e.text() === 'HomeHome') {
-        $e.text(txt);
-      } else {
-        $e.text('Home');
-      }
-    }
-    
+
+    // SLIDE DOWN navigation when down loading
+    setTimeout(function() {
+      $('nav').addClass('moveUp');
+    }, 600);
+
+
+    // MAIN NAV LINKS
     $('a.stockists').bind('click touch', function(e) {
       e.preventDefault(); e.stopPropagation();
       $('#stockists').toggleClass('extend');
@@ -28,6 +28,15 @@
         $('a.stockists').text('Stockists');
       }, 500);
     });
+
+    function toggleHome($e, txt) {
+      // there are 2 links (one hidden)
+      if ($e.text() === 'HomeHome') {
+        $e.text(txt);
+      } else {
+        $e.text('Home');
+      }
+    }
 
   });
 }).call(this);
