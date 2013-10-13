ce24c6f2cab9945d838494384d9799ae428c36c8
stefanRitter <stefan@stefanritter.com>
2013-10-13 16:58:47 +0100

    implement nav

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,7 +1,33 @@
 (function() {
   jQuery(function($) {
+
+    function toggleHome($e, txt) {
+      if ($e.text() == 'Home') {
+        $e.text(txt);
+      } else {
+        $e.text('Home');
+      }
+    }
     
-    console.log("loaded");
-    
+    $('a.stockists').bind('click touch', function(e) {
+      e.preventDefault(); e.stopPropagation();
+      $('#stockists').toggleClass('extend');
+      $('#info').removeClass('extend');
+      setTimeout(function() {
+        toggleHome($('a.stockists'), 'Stockists');
+        $('a.info').text('More Info');
+      }, 500);
+    });
+
+    $('a.info').bind('click touch', function(e) {
+      e.preventDefault(); e.stopPropagation();
+      $('#info').toggleClass('extend');
+      $('#stockists').removeClass('extend');
+      setTimeout(function() {
+        toggleHome($('a.info'), 'More Info');
+        $('a.stockists').text('Stockists');
+      }, 500);
+    });
+
   });
 }).call(this);
