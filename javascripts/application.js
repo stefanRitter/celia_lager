15fc53e57b253c4f43e95251e9001ece37fd5b92
stefanRitter <stefan@stefanritter.com>
2013-12-09 19:06:27 +0000

    update celia

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,6 +1,7 @@
 (function() {
+  'use strict';
+
   jQuery(function($) {
-    'use strict';
 
     checkCookie();
 
@@ -13,7 +14,8 @@
 
     // MAIN NAV LINKS
     $('a.stockists').bind('click touch', function(e) {
-      e.preventDefault(); e.stopPropagation();
+      e.preventDefault();
+      e.stopPropagation();
       $('#stockists').toggleClass('extend');
       $('#info, #buyOnline').removeClass('extend');
       setTimeout(function() {
@@ -23,7 +25,8 @@
     });
 
     $('a.info').bind('click touch', function(e) {
-      e.preventDefault(); e.stopPropagation();
+      e.preventDefault();
+      e.stopPropagation();
       $('#info').toggleClass('extend');
       $('#stockists, #buyOnline').removeClass('extend');
       setTimeout(function() {
@@ -33,7 +36,8 @@
     });
 
     $('a.buyOnline').bind('click touch', function(e) {
-      e.preventDefault(); e.stopPropagation();
+      e.preventDefault();
+      e.stopPropagation();
       $('#buyOnline').toggleClass('extend');
       $('#stockists, #info').removeClass('extend');
       $('a.info').text('More Info');
@@ -50,7 +54,7 @@
     }
 
     // MORE INFO NAV LINKS
-    $('#info ul li').on("click touch", function(e) {
+    $('#info ul li').on('click touch', function(e) {
       $('#info ul li').removeClass('active');
       var target = $(e.target).attr('id');
       $('.subNav').find('#'+target).addClass('active');
@@ -74,75 +78,44 @@
 
     // MAILCHIMP POPUP
     $('#noThanks').bind('click touch', function(e) {
-      e.preventDefault(); e.stopPropagation();
+      e.preventDefault();
+      e.stopPropagation();
       $('#newsletter').removeClass('extend');
     });
 
-    // SECIAL OFFER
-    $('.specialOffer').on('click touch', function(e) {
-      e.preventDefault(); e.stopPropagation();
-      $('#specialOfferPopup').addClass('extend');
-    });
-    $('#specialOfferPopup').on('click touch', function(e) {
-      $('#specialOfferPopup').removeClass('extend');
-    });
   });
 }).call(this);
 
 
 
-//cookie code is based on the w3school's cookie tutorial, comments are mine
-//setCookie is used to install a new cookie
+//cookie code is based on the w3school
 function setCookie(c_name, value, exdays) {
-
   var exdate = new Date();
-  //get system date&time
   exdate.setDate(exdate.getDate() + exdays);
-  //calculates the expiration date, adding exdays to the current date
-
-  var c_value = escape(value) + ((exdays == null) ? "" : "; expires=" + exdate.toUTCString());
-  //escape() makes the string stored in value ASCII confirm, ie no special characters
-  //example: c_value = "value string; expires=Wed, 21 Nov 2012 16:38:52 GMT"
-
-  document.cookie = c_name + "=" + c_value;
-  //writes out cookie:
-  //MyCookieName=MyCookieValue; expires=Wed, 21 Nov 2012 16:38:52 GMT
+  var c_value = escape(value) + ((exdays == null) ? '' : '; expires=' + exdate.toUTCString());
+  document.cookie = c_name + '=' + c_value;
 }
 
-//returns the value of a cookie
 function getCookie(c_name) {
-
   var i, x, y;
-  //counters and iteratorxs
-
   var ARRcookies = document.cookie.split(";");
-  //this creates an array of strings for all the arguments in the cookie
 
-  for ( i = 0; i < ARRcookies.length; i++) {//loop through the elements of the array
-
-    x = ARRcookies[i].substr(0, ARRcookies[i].indexOf("="));
-    //this extracts the name of cookie
-
-    y = ARRcookies[i].substr(ARRcookies[i].indexOf("=") + 1);
-    //this extracts the rest of the arguement after the equalsign
-
-    x = x.replace(/^\s+|\s+$/g, "");
-    //this deletes all white and empty spaces in the string, using RegExp
-
+  for ( i = 0; i < ARRcookies.length; i++) {
+    x = ARRcookies[i].substr(0, ARRcookies[i].indexOf('='));
+    y = ARRcookies[i].substr(ARRcookies[i].indexOf('=') + 1);
+    x = x.replace(/^\s+|\s+$/g, '');
     if (x == c_name) {
       return unescape(y);
-      //if this is what we are looking for then return the value
     }
   }
 }
 
 function checkCookie() {
-  var username = getCookie("hasbeenhere");
-
-  if (username != null && username != "") {
+  var username = getCookie('hasbeenhere');
+  if (username != null && username != '') {
     $('#newsletter').remove();
   } else {
-    setCookie("hasbeenhere", "yes", 4);
+    setCookie('hasbeenhere', 'yes', 4);
   }
 }
 //end of cookie code
